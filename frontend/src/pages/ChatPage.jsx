bed389124b2762936acf38885ee6c71960694b13
JuliePro <prokofevajuliya@gmail.com>
2025-04-24 12:08:52 +0300

    test

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -134,8 +134,8 @@ const ChatPage = () => {
               variant={channel.id === currentChannelId ? 'secondary' : 'light'}
               id={`dropdown-split-basic-${channel.id}`}
               className="border-0 rounded-0 rounded-end"
-            >
-              {t('chat.channelControl')}
+              aria-label={t('chat.channelControl')}
+             >
             </Dropdown.Toggle>
             <Dropdown.Menu>
               <Dropdown.Item onClick={() => handleShowModal('remove', channel)}>Удалить</Dropdown.Item>
